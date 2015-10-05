86d0d4b75730ad71a7f969718ee34275932f94de
Maxim Loginov <maxim.loginov@mail.ru>
2015-10-05 22:22:40 +0200

    fix output to file

--- run.sh
+++ run.sh
@@ -5,6 +5,10 @@ id >> result.dat
 cat /etc/*release* >> result.dat
 cat /proc/cpuinfo >> result.dat
 
-sudo add-apt-repository http://www.openfoam.org/download/ubuntu
+sudo add-apt-repository http://www.openfoam.org/download/ubuntu >> result.dat 2>&1
 sudo apt-get update
-sudo apt-get install openfoam240
+sudo apt-get install openfoam240 >> result.dat 2>&1
+
+ls -l /opt >> result.dat
+ls -l /opt/openfoam* >> result.dat 2>&1
+source /opt/openfoam*/etc/bashrc >> result.dat 2>&1
