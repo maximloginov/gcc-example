c03b85c7c3b5b7b80ddfad1143dc7517cfcfc158
Maxim Loginov <maxim.loginov@mail.ru>
2015-10-05 21:47:53 +0200

    try install openfoam

--- run.sh
+++ run.sh
@@ -4,3 +4,7 @@ uname -a > result.dat
 id >> result.dat 
 cat /etc/*release* >> result.dat
 cat /etc/proc >> result.dat
+
+sudo add-apt-repository http://www.openfoam.org/download/ubuntu
+sudo apt-get update
+sudo apt-get install openfoam240
